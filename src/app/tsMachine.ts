f175113bff5bc9c0f091b9ddedb2342a47dc6f46
goagain <remain.goagain@gmail.com>
2021-04-06 17:28:20 -0400

    Change StateMachine descriptions

--- src/app/tsMachine.ts
+++ src/app/tsMachine.ts
@@ -70,7 +70,7 @@ export const TS_Machine = Machine({
                 [TS_EVENTS.NO]: TS_STATES.NEED_ASSISTANT
             },
             meta: {
-                message: `You might have problem with your computer, this can be cause by Network Interface Card, or fire wall, or computer virus,etc. Could try these steps:
+                message: `You might have problem with your computer, this can cause by Network Interface Card, or firewall, or computer virus,etc. Please try these steps:
                 1. Restart browser or computer.
                 2. Turn off fire wall.
                 3. Scan virus.
@@ -86,7 +86,7 @@ export const TS_Machine = Machine({
                 [TS_EVENTS.NO]: TS_STATES.NEED_ASSISTANT,
             },
             meta: {
-                message: "The problem is modem, let's first try plug and unplug the cable, or replace the cable with other one.  Did you solve the problem?",
+                message: "The problem is modem, let's first try plug and unplug the cables that are connected to it, or replace the cable with other one. Did you solve the problem?",
                 picture: '2.png'
             }
         },
@@ -121,8 +121,7 @@ Did you solve your problem?`
                 [TS_EVENTS.NO]: TS_STATES.CANNOT_PING_GATEWAY,
             },
             meta: {
-                message: "Let's now ping gateway account. Can you ping through?",
-                //================= 3 张ping网关的图片 todo！！！================
+                message: "Please follow the steps below, we are trying find out whether the problem is from the gateway. Is there any active status ?",
                 pictures: ['ping1.png', 'ping2.png', 'ping3.png']
             }
         },
@@ -142,7 +141,7 @@ Did you solve your problem?`
                 [TS_EVENTS.NO]: TS_STATES.WIFI_CONNECT_DIRECTLY,
             },
             meta: {
-                message: 'Please try to restart the WIFI router,modem(if have), gataway device,and computer,    does it work now ?'
+                message: 'Please try to restart the WIFI router, modem(if have), gataway device and computer. Does it work now ?'
             }
         },
         [TS_STATES.WIFI_CONNECT_DIRECTLY]: {
@@ -151,7 +150,7 @@ Did you solve your problem?`
                 [TS_EVENTS.NO]: TS_STATES.OTHER_CANNOT_CONNECT,
             },
             meta: {
-                message: 'Can you try to use Ethernet cable directly plug into you computer?'
+                message: 'Let\'s now try to use Ethernet cable directly plug into you computer. Problem solved?'
             }
         },
         [TS_STATES.ADSL_DIAL]: {
